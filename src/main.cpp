ae2cbd4051d16c084616e13cc2be562cbfa9c149
bobroden <40324952+bobroden@users.noreply.github.com>
2021-04-15 11:53:28 +0300

    docs: add main cpp

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Automata.h"
+#include <string>
+using namespace std;
+
+int main() {
+	Automata CoffeeMachine = Automata();
+	CoffeeMachine.on();
+  CoffeeMachine.coin(30);
+	CoffeeMachine.choice(2);
+	CoffeeMachine.coin(15);
+	CoffeeMachine.choice(4);
+	CoffeeMachine.coin(50);
+	CoffeeMachine.choice(1);
+  CoffeeMachine.coin(25);
+	CoffeeMachine.choice(3);
+	CoffeeMachine.off();
+	return 0;
+}
