2aa0b578de4ddfd29335dcf6e18f81574f1f876d
bobroden <40324952+bobroden@users.noreply.github.com>
2021-04-15 12:00:15 +0300

    add: tests in test.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,2 +1,31 @@
 #include <gtest/gtest.h>
 #include "Automata.h"
+
+TEST(task, test1)
+{
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.coin(20);
+    coffee.choice(1);
+    int result = coffee.getState();
+    EXPECT_EQ(1, result);
+}
+
+TEST(task, test2)
+{
+    Automata coffee= Automata();
+    coffee.on();
+    coffee.off();
+    int result = coffee.getState();
+    EXPECT_EQ(0, result);
+}
+
+TEST(task, test3)
+{
+    Automata coffee = Automata();
+    coffee.on();
+    coffee.coin(20);
+    coffee.coin(10);
+    int result = coffee.getState();
+    EXPECT_EQ(2, result);
+}
